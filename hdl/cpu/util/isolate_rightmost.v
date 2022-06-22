1ae442f1f27258b4f6d1ab58859b02fefd712dac
Liao Junxuan <mikeljx@126.com>
2022-06-22 11:03:08 +0800

    isolate the rightmost bit

---
+++ hdl/cpu/util/isolate_rightmost.v
@@ -0,0 +1,12 @@
+module isolate_rightmost #
+    (
+        parameter WIDTH = 2
+    )
+    (
+        input [WIDTH-1:0] in,
+        output [WIDTH-1:0] out
+    );
+    // Isolate the rightmost bit
+    // http://fpgacpu.ca/fpga/Bitmask_Isolate_Rightmost_1_Bit.html
+    assign out = in & -in;
+endmodule
